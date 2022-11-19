40312accb6e104fb32c9e8210b1440b650fc206d
chingsky-liu <ivan33@163.com>
2022-11-19 04:39:03 +0000

    Updates: Prtoject-Telescopium Bringup
    Change-Id: I40f4fa7ee782371ae6debaebeff156dadaaa331c

--- app/src/main/java/org/pixelexperience/ota/misc/Constants.java
+++ app/src/main/java/org/pixelexperience/ota/misc/Constants.java
@@ -30,9 +30,9 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://api.pixelexperience.org/ota/%s/%s";
-    static final String OTA_CI_URL = "https://api.pixelexperience.org/ota_ci/%s/%s";
-    static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
-    static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
+    static final String OTA_URL = "https://raw.githubusercontent.com/Telescopium-Devices/ota/13/%s.json";
+    static final String OTA_CI_URL = "https://raw.githubusercontent.com/Telescopium-Devices/ota/13/%s.json";
+    static final String MAINTAINER_URL = "https://github.com/chingsky-liu";
+    static final String DOWNLOAD_WEBPAGE_URL = "https://raw.githubusercontent.com/Telescopium-Devices/Changelogs/thirteen/changelog.md";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
